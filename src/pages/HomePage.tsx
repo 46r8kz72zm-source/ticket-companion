d53885b2e58a830297f3d4bf35084a4e7ec7a45f
austincourson <austincourson@gmail.com>
2026-07-18 10:01:01 -0600

    Create HompePage component

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+function HomePage() {
+  return (
+    <main>
+      <h1>🚂 Ticket Companion</h1>
+
+      <p>
+        Play Ticket to Ride remotely using your own physical boards.
+      </p>
+
+      <div>
+        <button>Create Game</button>
+        <button>Join Game</button>
+      </div>
+    </main>
+  );
+}
+
+export default HomePage;
