b08caecb7c907c05fd39fc079b52c9dad40019ec
austincourson <austincourson@gmail.com>
2026-07-24 10:55:55 -0600

    Add initial engine test suite

--- src/core/tests/createDeck.test.ts
+++ src/core/tests/createDeck.test.ts
@@ -8,4 +8,34 @@ describe("createDeck", () => {
 
     expect(deck).toHaveLength(110);
   });
+
+  it("creates 14 locomotives", () => {
+    const deck = createDeck();
+
+    const locomotives = deck.filter(
+      (card) => card.color === "locomotive"
+    );
+
+    expect(locomotives).toHaveLength(14);
+  });
+
+  it("creates 12 red cards", () => {
+    const deck = createDeck();
+
+    const redCards = deck.filter(
+      (card) => card.color === "red"
+    );
+
+    expect(redCards).toHaveLength(12);
+  });
+
+  it("creates 12 blue cards", () => {
+    const deck = createDeck();
+
+    const blueCards = deck.filter(
+      (card) => card.color === "blue"
+    );
+
+    expect(blueCards).toHaveLength(12);
+  });
 });
